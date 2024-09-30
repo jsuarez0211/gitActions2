cf7955b3a45e661042f7712e4361966571b5afda
jsuarez0211 <jsuarez0211@gmail.com>
2024-09-30 19:25:05 -0400

    test actions 3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,19 @@
-// test/index.test.js
 const request = require("supertest");
 const app = require("../index");
 
+let server;
+
+beforeAll((done) => {
+    server = app.listen(3000, done); // Start the server
+});
+
+afterAll((done) => {
+    server.close(done); // Close the server
+});
+
 describe("GET /", () => {
     it("should return Hello World", async () => {
-        const res = await request(app).get("/");
+        const res = await request(server).get("/");
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe("Hello World");
     });
